66086694178126f6d6e660df97db359a90d41624
Rahul Muttineni <rahulmutt@gmail.com>
2019-04-04 23:59:08 +0530

    Fix ci (again)

--- scripts/install-scala.sh
+++ scripts/install-scala.sh
@@ -4,6 +4,6 @@ set -e
 
 SBT_VERSION=1.2.8
 apt-get update
-apt-get install curl
+apt-get install -y curl
 curl -fsL "https://github.com/sbt/sbt/releases/download/v$SBT_VERSION/sbt-$SBT_VERSION.tgz" | tar xfz - -C /usr/local
 ln -s /usr/local/sbt/bin/* /usr/local/bin/
